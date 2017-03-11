4a0a89bc68d428814b4f16fd0a4d9fe614ac2180
Aditya Shastri <ad.shastri1@gmail.com>
2017-03-11 15:12:25 -0500

    basic structure set up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var xml2js = require('xml2js');
+var parser = new xml2js.Parser({explicitArray: false});
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('home page');
+});
+
+router.get('/directions', function(req, res, next){
+
+  request({
+    url: 'http://ruevents.rutgers.edu/events/getEventsRss.xml',
+    method: 'GET',
+    qs: {keyword: 'Congressman'},
+  }, function (error, response, body){
+    if (error){
+      console.log(error);
+    } else {
+      parser.parseString(response.body, function (err, result){
+        res.send(result);
+      });
+
+    }
+  });
 });
 
 module.exports = router;
---
+++ utils/geo.js
@@ -0,0 +1,10 @@
+var googleMapsClient = require('@google/maps').createClient({
+  key: 'your API key here'
+});
+
+module.exports = {
+  location: function(name, callback){
+
+    callback(error, loc);
+  }
+}
